abee1102ce721277ead863f4fa43ac11af0651d7
ommar-hassan <102484131+ommar-hassan@users.noreply.github.com>
2024-10-16 14:52:28 +0300

    product repo

---
+++ ClientProductManager/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClientProductManager.Models;
+
+namespace ClientProductManager.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductAsync(Guid id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(Guid id);
+    }
+}
---
+++ ClientProductManager/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using ClientProductManager.Data;
+using ClientProductManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientProductManager.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(Guid id)
+        {
+            var product = await GetProductAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Product> GetProductAsync(Guid id)
+        {
+            if (id == Guid.Empty)
+                throw new ArgumentNullException(nameof(id));
+
+            return await _context.Products
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id) ?? throw new ArgumentException("Product not found");
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                .AsNoTracking()
+                .OrderBy(p => p.Name)
+                .ToListAsync();
+        }
+    }
+}
